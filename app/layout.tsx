0270d4ddec36b71c15d24f19687d48f1cdd0e6cc
arsindhurakar <arsindhurakar@gmail.com>
2025-11-10 15:28:19 +0545

    added tawk widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter, Outfit } from "next/font/google";
 import LayoutWrapper from "components/shared/LayoutWrapper";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,19 @@ export default function RootLayout({
     <html lang="en" className={`${inter.variable} ${outfit.variable}`}>
       <body className="antialiased min-h-screen flex flex-col justify-between">
         <LayoutWrapper>{children}</LayoutWrapper>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6911ab88ee2732195b2fc4cc/1j9mgc3ka';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
